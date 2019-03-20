b2279ae225ff8af56b312c08f1d72ea3d4a58e17
sonyali-org <lifenge@hotmail.com>
2019-03-20 21:42:05 +0000

    first pipeline

--- sources/calc.py
+++ sources/calc.py
@@ -18,7 +18,7 @@ def conv(value):
 
 # The 'add2' function itself
 def add2(arg1, arg2):
-    # Convert 'arg1' and 'arg2' to their appropriate types
+    # Convert 'arg1' and 'arg2' to their appropriate types - test one!
     arg1conv = conv(arg1)
     arg2conv = conv(arg2)
     # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
